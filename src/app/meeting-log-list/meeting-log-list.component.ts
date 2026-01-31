e50ed4b7da024be08c157e29c0659bac9b9a722a
ICPRplshelp <93059453+ICPRplshelp@users.noreply.github.com>
2026-01-30 22:14:19 -0500

    Ins. changes now show change before accordion expansion

--- src/app/meeting-log-list/meeting-log-list.component.ts
+++ src/app/meeting-log-list/meeting-log-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Meeting } from '../cinterfaces';
+import { InstructorLog, Meeting } from '../cinterfaces';
 
 @Component({
   selector: 'app-meeting-log-list',
@@ -12,6 +12,39 @@ export class MeetingLogListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  getChangeCount(insLog: InstructorLog | undefined): number {
+    if (insLog === undefined) {
+      return 0;
+    }
+    const firstIsEmpty = (insLog.initialInstructors.length === 0);
+    if(firstIsEmpty) {
+      return Math.max(0, insLog.instructorChanges.length - 1);
+    } else {
+      return Math.max(0, insLog.instructorChanges.length);
+    }
+  }
+
+  sq(t: any[] | number): string {
+    if (typeof t === 'number') {
+      if(t === 1) {
+        return "";
+      } else {
+        return "s";
+      }
+    }
+    if (t.length === 1) return "";
+    return "s";
+  }
+
+  noOnZero(num: number): string {
+    if(num === 0) {
+      return "No";
+    } else {
+      return `${num}`;
+    }
+  }
+
 
     getDeliverySymbol(deliveryMode: string | undefined): string {
     switch (deliveryMode) {
